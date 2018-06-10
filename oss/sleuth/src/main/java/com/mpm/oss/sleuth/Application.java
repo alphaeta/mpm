11ac10970a900c457dc61b0e4fff09a2b8e0fb9b
zhangzhongmin <47265575@qq.com>
2018-06-10 22:40:07 +0800

    sleuth

--- oss/sleuth/src/main/java/com/mpm/oss/sleuth/Application.java
+++ oss/sleuth/src/main/java/com/mpm/oss/sleuth/Application.java
@@ -2,11 +2,13 @@ package com.mpm.oss.sleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
