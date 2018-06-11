0f9c1bc3337aef0f3907c7d265d201fbaa36e677
Administrator <Administrator@gzqy09>
2018-06-11 15:42:55 +0800

    turbine

--- oss/turbine/src/main/java/com/mpm/oss/turbine/Application.java
+++ oss/turbine/src/main/java/com/mpm/oss/turbine/Application.java
@@ -2,10 +2,12 @@ package com.mpm.oss.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
+@EnableHystrixDashboard
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
